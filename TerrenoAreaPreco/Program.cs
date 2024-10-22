f005ad34a0cd885151482d40589e5004807b8968
Gabriela <gabriela.priaro@gmail.com>
2024-10-22 15:01:03 +0100

    Implementaçao calculo area e preço do terreno.

--- TerrenoAreaPreco/Program.cs
+++ TerrenoAreaPreco/Program.cs
@@ -4,13 +4,32 @@
 //o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com
 //duas casas decimais
 
+using System.Globalization;
+
 namespace TerrenoAreaPreco
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite a largura do terreno:");
+            double larguraTerreno, comprimentoTerreno, areaTerreno, valorMetroQuadrado, precoTerreno;
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Digite a largura do terreno: ");
+            larguraTerreno = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite o comprimento do terreno: ");
+            comprimentoTerreno = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Digite o valor do metro quadrado: ");
+            valorMetroQuadrado = double.Parse(Console.ReadLine(), CI);
+
+            areaTerreno = larguraTerreno * comprimentoTerreno;
+            precoTerreno = areaTerreno * valorMetroQuadrado;
+
+            Console.WriteLine($"Area do terreno = {areaTerreno.ToString("F2", CI)}");
+            Console.WriteLine($"Preco do terreno = {precoTerreno.ToString("F2", CI)}");
         }
     }
 }
